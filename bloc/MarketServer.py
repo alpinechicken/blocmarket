c5c934367bd0ccdab57ccecba1713a2190bf29ef
Admin <admin@Box.local>
2018-11-17 15:14:54 +0800

    DB_LOCATION to .env

--- bloc/MarketServer.py
+++ bloc/MarketServer.py
@@ -4,6 +4,7 @@ import numpy.matlib as npm
 import pandas as pd
 from sqlalchemy import create_engine, Table, Column, Integer, String, Float, \
     LargeBinary, BLOB, MetaData, update
+import os
 
 # Crypto imports
 import nacl.encoding
@@ -45,7 +46,7 @@ class MarketServer(object):
         # self.engine = create_engine("postgresql://alpine:3141592@localhost/blocparty",
         #                             isolation_level="AUTOCOMMIT")
         # Note postgress needs AUTOCOMMIT or else postgress hangs when it gets to a matching trade
-        self.engine = create_engine('sqlite:///pmarket.db')
+        self.engine = create_engine(os.environ.get('DB_LOCATION'))
         self.engine.echo = False
         self.metadata = MetaData(self.engine)
         self.userTable = Table('userTable', self.metadata,
