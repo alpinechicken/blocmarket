08fc7d3909c59082604b5c39a05db05bc09627d9
Admin <admin@Box.local>
2018-11-17 16:00:53 +0800

    bleh

--- bloc/MarketServer.py
+++ bloc/MarketServer.py
@@ -46,8 +46,8 @@ class MarketServer(object):
         # self.engine = create_engine("postgresql://alpine:3141592@localhost/blocparty",
         #                             isolation_level="AUTOCOMMIT")
         # Note postgress needs AUTOCOMMIT or else postgress hangs when it gets to a matching trade
-        # DATABASE_URL = 'sqlite:///pmarket.db'
-        DATABASE_URL = os.environ['DATABASE_URL']
+        DATABASE_URL = 'sqlite:///pmarket.db'
+        # DATABASE_URL = os.environ['DATABASE_URL']
         self.engine = create_engine(DATABASE_URL)
         self.engine.echo = False
         self.metadata = MetaData(self.engine)
